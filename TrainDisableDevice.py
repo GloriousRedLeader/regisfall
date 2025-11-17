1aeabdb490278fd237e160ecec22ce6b2faf2e4e
arturo the burro <arturotheburro7@gmail.com>
2025-11-16 22:39:23 -0800

    this script now works and no config needed

--- TrainDisableDevice.py
+++ TrainDisableDevice.py
@@ -3,30 +3,6 @@ print("Attempts to unlock nearby chest")
 from System.Collections.Generic import List
 from System import Byte, Int32
 import sys
-
-texts = Journal.GetTextByType("System")
-for text in texts:
-    print(text)
-    
-sys.exit()
-
-while True:
-    Journal.Clear()
-    
-    Misc.Pause(5000)
-    print(Journal.SearchByType("You dig some iron", "System"))
-    
-    
-    
-#texts = Journal.GetTextByType("System")
-#print(Journal.SearchByType("This does not", "System"))
-
-#for text in texts:
-#    print(text)
-    
-
-#sys.exit()
-
     
 while Player.GetSkillValue("Disable Device") < Player.GetSkillCap("Disable Device"):
     skill = Player.GetSkillValue("Disable Device")
@@ -56,24 +32,30 @@ while Player.GetSkillValue("Disable Device") < Player.GetSkillCap("Disable Devic
     if chests is None or len(chests) == 0:
         print("Stopping, need to be near a training chest")
         sys.exit()
-    
-    print(chests[0].Name)
 
     lockpicks = Items.FindByID(0x14FC,0,Player.Backpack.Serial,0)
     if lockpicks is None:
         print("Not enough lockpicks")
         sys.exit()
-    
-    
+        
+    # Re-lock the box because it is currently letting us see the third property
+    # "0 Items, 0 Stones"
+    if len(chests[0].Properties) == 3:
+        filter = Items.Filter()
+        filter.OnGround = True
+        filter.RangeMax = 2
+        filter.Graphics = List[Int32]([0x100E]) 
+        keys = Items.ApplyFilter(filter)
+        key = next(( k for k in keys if chestName in Items.GetPropStringByIndex(k.Serial, 2).lower() ), None  )
+        
+        Items.UseItem(key)
+        Target.WaitForTarget(3000)
+        Target.TargetExecute(chests[0])
+        Misc.Pause(100)
+
     Items.UseItem(lockpicks)
     Target.WaitForTarget(3000)
-    Target.TargetExecute(chests[0])   
-    #if Journal.Search("This does not appear"):
-    if Journal.SearchByType("This does not appear", "System"):
-        print("NOT LOCKED")
-    else:
-        print("Locked")
-
+    Target.TargetExecute(chests[0])
+    Misc.Pause(3000)
     
-    print("Waiting")
-    Misc.Pause(5000)
+    continue 
